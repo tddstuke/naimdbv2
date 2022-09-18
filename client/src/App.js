bbc5f5ee74d4d2d75ee5f8c40275a393f7611d41
Todd Stuke <tddstuke@gmail.com>
2022-09-18 15:17:13 -0500

    navbar skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState } from "react";
+import NavBar from "./components/Header";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <NavBar />;
 }
 
 export default App;

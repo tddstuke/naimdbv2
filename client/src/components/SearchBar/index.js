5dbf566721cd0c7eaa2381eb286fef157256528f
Todd Stuke <tddstuke@gmail.com>
2022-09-20 19:32:39 -0500

    added state and change handlers for search bars

--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SearchBar = () => {
+  const [movieTitle, setMovieTitle] = useState("");
+  const [showTitle, setShowTitle] = useState("");
+
+  const handleMovieChange = (event) => {
+    setMovieTitle(event.target.value);
+  };
+  const handleShowChange = (event) => {
+    setShowTitle(event.target.value);
+  };
+
+  const handleMovieSubmit = async (event) => {
+    event.preventDefault();
+    await console.log(movieTitle);
+    setMovieTitle("");
+  };
+
+  const handleShowSubmit = async (event) => {
+    event.preventDefault();
+    await console.log(showTitle);
+    setShowTitle("");
+  };
+
   return (
     <div className=" flex flex-col justify-center items-center bg-black">
-      <form className="w-full max-w-lg">
+      <form className="w-full max-w-lg" onSubmit={handleMovieSubmit}>
         <div className="md:flex md:items-center mb-6">
           <div className="md:w-1/3">
             <label className="block text-white md:text-right mb-1 md:mb-0 pr-4 ">
@@ -16,17 +38,19 @@ const SearchBar = () => {
               id="inline-full-name"
               type="text"
               placeholder="Movie Title"
+              value={movieTitle}
+              onChange={handleMovieChange}
             />
           </div>
           <button
-            class="px-4 py-2 text-white bg-gray-600 rounded-md shadow hover:bg-gray-800 md:mt-0 mt-2 md:ml-2 md:mt-0 mt-2 md:w-1/5 w-full"
-            type="button"
+            class="px-4 py-1 text-white bg-gray-600 rounded-md shadow hover:bg-gray-800 md:mt-0 mt-2 md:ml-2 md:mt-0 mt-2 md:w-1/5 w-full"
+            type="submit"
           >
             Search
           </button>
         </div>
       </form>
-      <form className="w-full max-w-lg">
+      <form className="w-full max-w-lg" onSubmit={handleShowSubmit}>
         <div className="md:flex md:items-center mb-6">
           <div className="md:w-1/3">
             <label className="block text-white md:text-right mb-1 md:mb-0 pr-4">
@@ -39,11 +63,13 @@ const SearchBar = () => {
               id="inline-full-name"
               type="text"
               placeholder="Show Title"
+              value={showTitle}
+              onChange={handleShowChange}
             />
           </div>
           <button
-            class="px-4 py-2 text-white bg-gray-600 rounded-md shadow hover:bg-gray-800 md:ml-2 md:mt-0 mt-2 md:w-1/5 w-full"
-            type="button"
+            class="px-4 py-1 text-white bg-gray-600 rounded-md shadow hover:bg-gray-800 md:ml-2 md:mt-0 mt-2 md:w-1/5 w-full"
+            type="submit"
           >
             Search
           </button>

6b99e8858fa42bdd145b57a96fd5f5b2a03c2fc3
Todd Stuke <tddstuke@gmail.com>
2022-10-02 14:54:17 -0500

    added where to watch fetch and render

--- client/src/pages/SingleMovie.js
+++ client/src/pages/SingleMovie.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import http from "../http-common";
-import movieService from "../utils/movie-service";
+import useFetch from "react-fetch-hook";
 
 const SingleMovie = () => {
+  const key = process.env.REACT_APP_API_KEY;
   const { id: movieId } = useParams();
   const [movie, setMovie] = useState("");
 
@@ -16,15 +17,23 @@ const SingleMovie = () => {
     getMovie();
   }, []);
 
+  const { isLoading, data } = useFetch(
+    `https://api.themoviedb.org/3/movie/${movieId}/watch/providers?api_key=${key}`
+  );
+  if (isLoading) {
+    return <div>loading...</div>;
+  }
+  const stream = data.results.US.flatrate;
+
   return (
     <>
       <div className="flex max-w-full md:justify-around justify-center flex-wrap ">
         <div
           // key={movie.id}
           // onClick={clickMovie}
-          className="md:w-full m-4 rounded overflow-hidden md:shadow-2xl md:justify-around justify-center md:flex"
+          className="md:w-4/5 m-4 rounded overflow-hidden md:shadow-2xl  justify-center md:flex"
         >
-          <div className="p-3 md:w-1/2">
+          <div className="p-3 justify-center flex md:w-1/2">
             <img
               id={movie.id}
               src={`http://image.tmdb.org/t/p/w500${movie.poster_path}`}
@@ -34,15 +43,30 @@ const SingleMovie = () => {
           </div>
           <div className="md:w-1/2">
             <ul className="m-5 space-y-5">
-              <li className="">Summary: {movie.overview}</li>
-              <li className="">Released on: {movie.release_date}</li>
               <li className="">
-                Homepage: <a href={movie.homepage}>{movie.title}</a>
+                <span className="font-bold">Summary: </span> {movie.overview}
               </li>
               <li className="">
-                Genres:
-                {movie.genres &&
-                  movie.genres.map((genre) => " " + genre.name + " ")}
+                <span className="font-bold">Release Date: </span>{" "}
+                {movie.release_date}
+              </li>
+              <li className="">
+                <span className="font-bold">Hompage Link: </span>
+                <a
+                  className="underline text-blue-600 hover:text-blue-800"
+                  href={movie.homepage}
+                >
+                  {movie.title}
+                </a>
+              </li>
+              <li className="">
+                <span className="font-bold">Genres: </span>
+                {movie.genres?.map((genre) => " " + genre.name + ", ")}
+              </li>
+              <li className="">
+                <span className="font-bold">Available to Stream: </span>
+                {stream?.map((network) => " " + network.provider_name + ", ") ||
+                  "Not available to stream for flatrate"}
               </li>
             </ul>
           </div>

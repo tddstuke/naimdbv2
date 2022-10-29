13337fd76bbf807904ba64c13d469b097ec8fcc7
Todd Stuke <tddstuke@gmail.com>
2022-10-29 17:44:34 -0500

    working on heroku bugs

--- server/server.js
+++ server/server.js
@@ -22,11 +22,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "../client/dist")));
 }
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
 sequelize.sync({ force: false }).then(() => {

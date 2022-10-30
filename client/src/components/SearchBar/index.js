8ca255df49b3dc2a2b657c62a1ace9ccff377a17
Todd Stuke <tddstuke@gmail.com>
2022-10-29 22:37:09 -0500

    submit fix for search bar requiring revamped css

--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -50,60 +50,59 @@ const SearchBar = () => {
 
   return (
     <div className=" flex flex-col justify-center items-center bg-black">
-      <form
-        className="w-full max-w-xlg px-4"
-        // onSubmit={handleMovieSubmit && handleShowSubmit}
-      >
-        <div className="md:flex md:items-center md:justify-center mb-6 md:mr-8">
-          <div className="md:w-1/4">
-            <label className="block text-white md:text-right mb-1 md:mb-0 pr-4 ">
-              Enter Movie Title:
-            </label>
-          </div>
-          <div className="md:w-2/3">
-            <input
-              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-1 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
-              id="movie "
-              type="text"
-              placeholder="Movie Title"
-              value={movieTitle}
-              onChange={handleMovieChange}
-            />
-          </div>
-          {/* <Link to={`/movieid/${movieId}`}> */}
-          <button
-            className="px-4 py-1 text-white bg-gray-600 rounded-md shadow hover:bg-gray-800 md:mt-0 mt-2 md:ml-2 md:mt-0 mt-2 md:w-1/5 w-full"
-            type="submit"
-            onClick={handleMovieSubmit}
-          >
-            Search
-          </button>
-          {/* </Link> */}
-
-          <div className="md:w-1/4">
-            <label className="block text-white md:text-right mb-1 md:mb-0 pr-4">
-              Enter Show Title:
-            </label>
-          </div>
-          <div className="md:w-2/3">
-            <input
-              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-1 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
-              id="inline-full-name"
-              type="text"
-              placeholder="Show Title"
-              value={showTitle}
-              onChange={handleShowChange}
-            />
-          </div>
-          <button
-            className="px-4 py-1 text-white bg-gray-600 rounded-md shadow hover:bg-gray-800 md:ml-2 md:mt-0 mt-2 md:w-1/5 w-full"
-            type="submit"
-            onClick={handleShowSubmit}
-          >
-            Search
-          </button>
+      <div className="w-full max-w-xlg px-4">
+        <div className="lg:flex lg:items-center lg:justify-center mb-6 md:mr-8">
+          <form className="w-full lg:flex">
+            <div className="lg:w-1/4">
+              <label className="block text-white lg:text-right mb-1 md:mb-0 pr-4 ">
+                Enter Movie Title:
+              </label>
+            </div>
+            <div className="lg:w-2/3">
+              <input
+                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-1 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
+                id="movie "
+                type="text"
+                placeholder="Movie Title"
+                value={movieTitle}
+                onChange={handleMovieChange}
+              />
+            </div>
+            {/* <Link to={`/movieid/${movieId}`}> */}
+            <button
+              className="px-4 py-1 text-white bg-gray-600 rounded-md shadow hover:bg-gray-800 lg:mt-0 mt-2 lg:ml-2 lg:mt-0 lg:mb-3 mt-2 lg:w-1/5 w-full"
+              type="submit"
+              onClick={handleMovieSubmit}
+            >
+              Search
+            </button>
+          </form>
+          <form className="w-full lg:flex">
+            <div className="lg:w-1/4">
+              <label className="block text-white lg:text-right mb-1 lg:mb-0 pr-4">
+                Enter Show Title:
+              </label>
+            </div>
+            <div className="lg:w-2/3">
+              <input
+                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-1 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
+                id="inline-full-name"
+                type="text"
+                placeholder="Show Title"
+                value={showTitle}
+                onChange={handleShowChange}
+              />
+            </div>
+            <button
+              className="px-4 py-1 text-white bg-gray-600 rounded-md shadow hover:bg-gray-800 lg:ml-2 lg:mt-0 lg:mb-3 mt-2 lg:w-1/5 w-full"
+              type="submit"
+              onClick={handleShowSubmit}
+            >
+              Search
+            </button>
+          </form>
         </div>
-      </form>
+      </div>
       {/* <form className="w-full max-w-md" onSubmit={handleShowSubmit}>
         <div className="md:flex md:items-center mb-6">
           <div className="md:w-1/3">

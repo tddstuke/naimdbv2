367ca82ec1dbb9b6df7baccd44f1fe5c7f08fb3d
Todd Stuke <tddstuke@gmail.com>
2022-10-29 19:29:47 -0500

    still working on bugs

--- server/server.js
+++ server/server.js
@@ -25,11 +25,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../client/build")));
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 sequelize.sync({ force: false }).then(() => {

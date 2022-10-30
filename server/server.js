cf42218bf0eee356fcf2514d215eb929c6d9a224
Todd Stuke <tddstuke@gmail.com>
2022-10-29 20:54:46 -0500

    still working on heroku

--- server/server.js
+++ server/server.js
@@ -28,9 +28,13 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now Listening on ${PORT}`));

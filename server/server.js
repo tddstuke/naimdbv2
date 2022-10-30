482110e1118155609d19f693ac0c927db5e45b6b
Todd Stuke <tddstuke@gmail.com>
2022-10-29 22:46:02 -0500

    trying another fix

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "..", "/client/build")));
 }
 
 app.get("/*", (req, res) => {

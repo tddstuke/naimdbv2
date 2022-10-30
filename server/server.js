1bae91933783c0dc37a04bdfff22ada546c71e2d
Todd Stuke <tddstuke@gmail.com>
2022-10-30 12:09:17 -0500

    not sure

--- server/server.js
+++ server/server.js
@@ -21,8 +21,8 @@ app.use(express.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(routes);
 }
-app.use(routes);
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));

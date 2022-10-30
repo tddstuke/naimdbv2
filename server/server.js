8111bb819197a6d4eeec89e36d9bb5ad4f21c393
Todd Stuke <tddstuke@gmail.com>
2022-10-29 21:03:06 -0500

    trying this

--- server/server.js
+++ server/server.js
@@ -28,13 +28,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.get("/", function (req, res) {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
-
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-// });
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now Listening on ${PORT}`));

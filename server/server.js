3c40848a699de72eed8adca2bc1db1775820c0ce
Todd Stuke <tddstuke@gmail.com>
2022-10-30 11:35:35 -0500

    homepage working but no other pages

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.use(routes);
+// app.use(routes);
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));

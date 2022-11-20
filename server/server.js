b222eef559f17a935777379caa36380f958907ec
Todd Stuke <tddstuke@gmail.com>
2022-11-19 18:09:45 -0600

    omg

--- server/server.js
+++ server/server.js
@@ -21,13 +21,9 @@ app.use(express.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
-  app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-  });
 }
 app.use(routes);
 
-app.use(express.static(path.join(__dirname, "/client/public"))); //new code
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });

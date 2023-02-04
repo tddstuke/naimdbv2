302182e6eacee67f845f9e2fc9adf1862f224c4b
Todd Stuke <tddstuke@gmail.com>
2023-02-04 17:50:08 -0600

    getting closer

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   // baseURL: "http://localhost:3001/api",
-  baseURL: "/api",
+  baseURL: "https://naimdb.herokuapp.com/api",
   headers: {
     "Content-type": "application/json",
   },

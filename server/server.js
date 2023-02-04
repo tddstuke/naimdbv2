069b11c95553ea8676ead1c208cb0798da41ff4f
Todd Stuke <tddstuke@gmail.com>
2023-02-04 17:57:24 -0600

    try again

--- server/server.js
+++ server/server.js
@@ -19,11 +19,11 @@ app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/api", routes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
-app.use("/api", routes);
 
 app.use("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));

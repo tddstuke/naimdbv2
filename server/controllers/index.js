4d2fb88ea439715eb036f6cd7c80a3ab1d7a4bca
Todd Stuke <tddstuke@gmail.com>
2023-02-04 18:00:00 -0600

    getting real close...

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
-const apiRoutes = require("./api");
-router.use("/api", apiRoutes);
+const apiRoutes = require("/");
+router.use("/", apiRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
